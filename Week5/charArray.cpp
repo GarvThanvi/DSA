832b40197880ef0d2e6c147c9f7d956ef2e54fbd
GarvThanvi <gtuhm2004@gmail.com>
2025-01-02 11:36:22 +0530

    char array , strings basic

---
+++ Week5/charArray.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int getLength(char arr[]){
+    int count = 0;
+    int i =0;
+    while(arr[i] != '\0'){
+        count++;
+        i++;
+    }
+    return count;
+}
+
+void replaceCharacter(char originalChar, char newChar, char arr[], int size){
+    for(int i=0; i<size;i++){
+        if(arr[i] == originalChar){
+            arr[i] = newChar;
+        }
+    }
+}
+
+void convertToUpperCase(char arr[], int n){
+    for(int i=0; i<n; i++){
+        if(arr[i] >= 'a' && arr[i] <= 'z' ){
+            arr[i] = arr[i] - 'a' + 'A'; //-32
+        }
+    }
+}
+
+void convertToLowerCase(char arr[], int n){
+    for(int i=0; i<n; i++){
+        if(arr[i] >= 'A' && arr[i] <= 'Z'){
+            arr[i] = arr[i] - 'A' + 'a'; //+32
+        }
+    }
+}
+
+void reverseArray(char arr[], int size){
+    int i=0, j=size-1;
+    while(i <= j){
+        swap(arr[i], arr[j]);
+        i++, j--;
+    }
+}
+
+bool checkPalindrome(char arr[], int size){
+    int i =0, j = size-1;
+    while(i <= j){
+        if(arr[i] == arr[j]){
+            i++,j--;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    //CHARACTER ARRAYS
+    // // char arr[100];
+    // // cin.getline(arr, 100, '\t');
+    // // cout<<arr;
+    
+    // char arr[100];
+    // cin>>arr;
+    // int size = getLength(arr);
+    // // replaceCharacter('@', ' ', arr, size);
+    // // convertToLowerCase(arr, size);
+    // // convertToUpperCase(arr,size);
+    // // reverseArray(arr,size);
+    // // if(checkPalindrome(arr,size)) cout<<"Yes it is palindrome"<<endl;
+    
+    // cout << arr;
+    //---------------------------------------------------------------------------------------------------------------------------
+    //CHAR ARRAYS FUCNTIONS
+    char arr1[100];
+    char arr2[100];
+    cin>>arr1;
+cin>>arr2;
+
+    cout << strcat(arr1, arr2)<<endl;
+    cout<<strlen(arr1)<<endl;
+    return 0;
+}
---
+++ Week5/strings.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // string str;
+    // cin >> str;
+    // cout << str << endl;
+    // // also you can str.push_back and str.pop_back
+    // string sentence;
+    // getline(cin, sentence, '\t'); // take input sentences
+    // cout << sentence;
+
+    //STRING STL
+    string str = "Garv Thanvi";
+    cout << str[0]<<endl;
+    cout << str.at(0)<<endl;
+    //str.front(), .back(), .length()
+    auto it = str.begin();
+    while(it != str.end()){
+        cout << *it;
+        it++;
+    }
+    //string = str1 + str2 //concatenation
+    //str.empty()
+    string sentence = "Hello, How are you?";
+    string word = "your";
+    cout<<endl<< sentence.substr(0,5);
+    cout << endl << sentence.substr(5);
+    int ans = sentence.find(word); //return index or -1
+    cout<<ans<<endl;
+
+    if(sentence.find(word) != string::npos){
+        cout<<"Found";
+    }else{
+        cout<<"Not Found";
+    }
+    //str.compare(str1) //boolean value
+    return 0;
+}
